714521157291c42c77f038ee4b611bef4d81e6f7
Adnan Alam <adnan.alam@rfera.asia>
2022-06-24 11:10:58 +0600

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="echologyx_py",
+    version="1.0.0",
+    description="A Python package containing two methods move_alphabets and missing_number",
+    url="https://github.com/adnan-alam/echologyx-py-package",
+    author="Adnan Alam",
+    author_email="adnan.alamcu@gmail.com",
+    packages=["echologyx_py"],
+    classifiers=[
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
