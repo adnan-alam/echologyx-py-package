0971babaf76c2f8871c81ad4120c9debcbd8a05c
Adnan Alam <adnan.alam@rfera.asia>
2022-06-24 11:10:42 +0600

    added echologyx_py module

---
+++ echologyx_py/__init__.py
@@ -0,0 +1,8 @@
+"""
+echologyx_py
+
+A Python package containing two methods move_alphabets and missing_number.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Adnan Alam"
---
+++ echologyx_py/echologyx_py.py
@@ -0,0 +1,35 @@
+def move_alphabets(word, alphabet):
+    """
+    Move all characters in the word which matches 'alphabet' to end of the word
+    while maintaining the relative order of the other characters in the word.
+
+    Args:
+        word (str): A string between 'a-z' (inclusive)
+        alphabet (str): A character between 'a-z' (inclusive).
+
+    Return:
+        word(str): Final result where all the matched alphabets moved to end of the word
+    """
+
+    if len(word) <= 1:
+        return word
+    elif word[0] == alphabet:
+        return move_alphabets(word[1:], alphabet) + word[0]
+    else:
+        return word[0] + move_alphabets(word[1:], alphabet)
+
+
+def missing_number(nums):
+    """
+    Return the only number in the range that is missing from the array.
+
+    Args:
+        nums (list): An array containing n distinct numbers in the range [0, n]. 'n' is length of the array.
+
+    Return:
+        result(int): An integer, which is the missing number from the given array.
+    """
+
+    n = len(nums)
+    result = list(set(range(n + 1)).difference(set(nums)))[0]
+    return result
